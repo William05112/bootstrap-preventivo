9751450f463bd9013fa53c236a1d10f13fbe386e
William05112 <liamscervino@gmail.com>
2025-04-07 15:41:09 +0200

    feat: form completato

--- jss/script.js
+++ jss/script.js
@@ -1,11 +1,11 @@
 const form = document.querySelector("form");
 console.log(form);
 
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
     event.preventDefault();
 
     const tipoLavoro = document.getElementById("tipo-di-lavoro").value;
-    
+
     // Se non è stato selezionato un tipo di lavoro, esci dalla funzione
     if (!tipoLavoro) {
         console.log("Seleziona un tipo di lavoro!");
@@ -24,16 +24,48 @@ form.addEventListener("submit", function(event) {
         tariffa = 33.60; // Project Analysis
     }
 
+
     // Calcola il totale
-    let totale = tariffa * oreLavoro;
+    let totalebase = tariffa * oreLavoro;
+
+    // Calcolo sconto
+    const codiciValidi = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+    const codicePromozionaleTagliato = document.getElementById("codice-promozionale").value.trim();
+
+    let sconto = 0;
+
+    for (let i = 0; i < codiciValidi.length; i++) {
+        if (codiciValidi[i] === codicePromozionaleTagliato) {
+            sconto = totalebase * 0.25;  // Applica il 25% di sconto
+            break;  // Esce dal ciclo se il codice è valido
+        }
+    }
+    // Calcola il totale finale dopo aver applicato eventualmente lo sconto
+    let totale = totalebase - sconto;  // Applica lo sconto al totale base
     let totalearrotondato = totale.toFixed(2);
 
     // Mostra il risultato nel paragrafo "importo-prezzo"
     const risultatoPrezzo = document.getElementById("importo-prezzo");
     risultatoPrezzo.textContent = `€ ${totalearrotondato}`;
-   
-   
-    // Reset del form dopo invio
+
+    if (sconto === 0 && codicePromozionaleTagliato !== "") {
+        alert("CODICE ERRATO");
+    }
+
+// In caso si voglia implementare un messaggio permanente in pagina ho creato un ciclo if con ilmessaggio collegato al paragrafo, al momento lo tengo commentato perchè ho preferito comunicare solo l'alert con il codice errato e avere i valori che si azzerano al submit del form
+
+    // const messaggioCodice = document.getElementById("messaggio-codice");
+
+    // if (codicePromozionaleTagliato === "") {
+    //     messaggioCodice.textContent = "";
+    // } else if (sconto > 0) {
+    //     messaggioCodice.textContent = "Codice valido!";
+    // } else {
+    //     messaggioCodice.textContent = "Codice errato!";
+    //     alert(`CODICE ERRATO`)
+    // }
+
+    // Reset del form dopo invio in caso il form ad ogni invio ricominci da zero (opzione che posso eliminare e lasciare i valori inseriti in pagina)
     const selectTipoLavoro = document.getElementById("tipo-di-lavoro");
     selectTipoLavoro.selectedIndex = 0
 
